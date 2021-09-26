8bab549493f3b31d7cd0234c7924a2ab3bf4025c
UdayakumarU <erudayu@gmail.com>
2021-09-26 23:05:22 +0530

    adding build version for deployment

---
+++ build/precache-manifest.99060b81f7073eaef9e4c2e653ca49f4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "798c88c8a657327b47dd0fd1beb51724",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4f285630beb60d611bae",
+    "url": "/static/css/2.a5f06b7f.chunk.css"
+  },
+  {
+    "revision": "d067d20c2622867c636a",
+    "url": "/static/css/main.7b6e377d.chunk.css"
+  },
+  {
+    "revision": "4f285630beb60d611bae",
+    "url": "/static/js/2.80d2b058.chunk.js"
+  },
+  {
+    "revision": "33490309eac47256b827830c802fd4a5",
+    "url": "/static/js/2.80d2b058.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d067d20c2622867c636a",
+    "url": "/static/js/main.96731e56.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
